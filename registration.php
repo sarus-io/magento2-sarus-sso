c42408c0cb384b493856b85b3d55d178e943b047
Yurii Torbyk <mail@yurii.me>
2018-06-18 14:02:10 +0300

    Init module

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Sarus, LLC. All rights reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Sarus_SsoIdp', __DIR__);
